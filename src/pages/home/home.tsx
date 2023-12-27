3d5f761bc0b62570569a8f4b0d353981b34791ae
Roohollah Mousavi <s.r.mousavi115@gmail.com>
2023-12-27 18:21:41 +0330

    feat: complete home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@components/shared/header'
+import { ChatList } from './components/chat-list'
 
 const Home = () => {
     return (
         <>
             <Header title="Chats" />
+            <ChatList />
         </>
     )
 }
