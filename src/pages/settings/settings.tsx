84578ca78d84e9728cd8a9992256f945f2e964a0
Roohollah Mousavi <s.r.mousavi115@gmail.com>
2023-12-27 18:26:11 +0330

    feat: add settings page

---
+++ src/pages/settings/settings.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@components/shared/header'
+
+const Settings = () => {
+    return (
+        <>
+            <Header title="Settings" />
+            Settings
+        </>
+    )
+}
+
+export default Settings
