d60e73f0b6f870dff628632359cadf8040d7a075
Roohollah Mousavi <s.r.mousavi115@gmail.com>
2023-12-27 18:20:37 +0330

    feat: complete home page

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@components/shared/header'
+
+const Home = () => {
+    return (
+        <>
+            <Header title="Chats" />
+        </>
+    )
+}
+
+export default Home
